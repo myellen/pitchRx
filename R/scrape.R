dcec0e1ec8a1027185e48268d46e963562f8bcfb
myellen <max.yellen@gmail.com>
2017-01-15 19:24:19 -0500

    change cap size

--- R/scrape.R
+++ R/scrape.R
@@ -215,13 +215,14 @@ scrape <- function(start, end, game.ids, suffix = "inning/inning_all.xml", conne
     inning.files <- paste0(gameDir, "/inning/inning_all.xml")
     n.files <- length(inning.files)
     #cap the number of files to be parsed at once (helps avoid exhausting memory)
-    cap <- min(200, n.files)
+    cap <- min(100000, n.files) #200
     if (n.files > cap && missing(connect)) {
       warning("play-by-play data for just the first 200 games will be returned (even though you've asked for", n.files, ")",
               "If you want/need more, please consider using the 'connect' argument.")
     }
     n.loops <- ceiling(n.files/cap)
     for (i in seq_len(n.loops)) {
+      print(sprintf("%d of %d", i, n.loops))
       #grab subset of files to be parsed
       inning.filez <- inning.files[seq(1, cap)+(i-1)*cap]
       inning.filez <- inning.filez[!is.na(inning.filez)]
